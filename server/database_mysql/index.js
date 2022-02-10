e2cba3560c83235254e7dc8b8eebe92e8636ce0c
Mortadha-Abidi <mortadha.abidi09@gmail.com>
2022-02-10 16:09:11 +0100

    backend-admin done

--- server/database_mysql/index.js
+++ server/database_mysql/index.js
@@ -3,7 +3,7 @@ var mysql = require('mysql2');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'Youssef123@',
+  password : 'root',
   database : 'delivery'
 });
 
